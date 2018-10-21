d0c4e77b1a9bcf6f17f3908eb7d7903ac5b64804
Marco Perez <mperez@anexinet.com>
2018-10-20 19:36:40 -0500

    Subo la estructura del proyecto al repositorio de GitHub

---
+++ GamesAndToys/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GamesAndToys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
